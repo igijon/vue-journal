7368b1012c8c0d2acfb3b51c957fddae6c64260c
Inma Gijón <igijoninf@gmail.com>
2022-05-19 18:12:09 +0200

    Estructura DayBook component

---
+++ src/modules/daybook/components/DaybookEntryList.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>EntryList!!!</h1>
+</template>
+<script>
+export default {
+    
+}
+</script>
--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1>Daybook Layout</h1>
+    <DaybookNavbar />
+    <div class="d-flex">
+        <div class="col-4">
+            <DaybookEntryList />
+        </div>
+        <div class="col">
+            <router-view />
+        </div>
+    </div>
 </template>
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+    components: {
+        DaybookNavbar: defineAsyncComponent(() => import("../components/DaybookNavbar.vue")),
+        DaybookEntryList: defineAsyncComponent(() => import("../components/DaybookEntryList.vue"))
+    }
+}
+</script>
