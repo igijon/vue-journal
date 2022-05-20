08a15fb5fa8ccec5af8ad52c0d6dcd6d470fbcb8
Inma Gijón <igijoninf@gmail.com>
2022-05-20 11:32:12 +0200

    Primera aproximación DaybookEntryList

--- src/modules/daybook/components/DaybookEntryList.vue
+++ src/modules/daybook/components/DaybookEntryList.vue
@@ -1,8 +1,38 @@
 <template>
-    <h1>EntryList!!!</h1>
+    <div class="entry-list-container">
+        <div class="px-2 pt-2">
+            <input 
+                type="text"
+                class="form-control"
+                placeholder="buscar entrada"
+            />  
+        </div>
+        <div class="entry-scrollarea">
+            <h2
+             v-for="item in 100"
+             :key="item">
+             Entry
+            </h2>
+        </div>
+    </div>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
+
+<style lang="scss" scoped>
+input {
+    height: 25px;
+}
+.entry-list-container{
+    border-right: 1px solid #2c3e50;
+    height: calc(100vh - 56px);
+}
+
+.entry-scrollarea {
+    height: calc(100vh - 110px);
+    overflow: scroll;
+}
+</style>
