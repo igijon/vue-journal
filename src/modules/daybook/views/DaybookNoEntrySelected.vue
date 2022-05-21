65f938d488d4839feaffdef2973712b6a1efe0b9
Inma Gijón <igijoninf@gmail.com>
2022-05-21 09:20:21 +0200

    Botón flotante para añadir entradas

--- src/modules/daybook/views/DaybookNoEntrySelected.vue
+++ src/modules/daybook/views/DaybookNoEntrySelected.vue
@@ -2,11 +2,15 @@
   <div class="d-flex justify-content-center">
       <h1 class="align-self-center">No hay nada seleccionado</h1>
   </div>
+  <Fab/>
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue'
 export default {
-
+    components: {
+        Fab: defineAsyncComponent(() => import('../components/DaybookFab.vue'))
+    }
 }
 </script>
 
